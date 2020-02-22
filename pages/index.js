3d46c25a7c63c2f17305f33548a0c2401937278b
A Big Macbook <jeffrey@cowbellcyber.ai>
2020-02-22 02:45:22 -0800

    Basic setup of fullpage.js, particle.js

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,34 @@
-import Head from 'next/head'
-import Typewriter from 'typewriter-effect';
+import React from 'react'
+import ReactFullpage from '@fullpage/react-fullpage';
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Jeffrey Lin</title>
-      <link rel="icon" href="/favicon.ico" />
-      <link
-          href="https://fonts.googleapis.com/css?family=Fira+Sans:400,400i,700&display=swap"
-          rel="stylesheet"
-      />
-    </Head>
+import MyFont from '../components/font.js';
+import MyBackground from '../components/background.js';
+import Homepage from '../components/homepage/homepage.js';
 
-    <main>
-      <Typewriter
-        options={{
-          strings: ['site under construction.'],
-          autoStart: true,
-          loop: true,
+class Home extends React.Component {
+  componentDidMount () {
+    MyFont()
+    MyBackground() // TO DO: animated background with color changes
+  }
+
+  render () {
+    return (
+      <ReactFullpage
+ 
+        licenseKey = {'1403168F-F2F146E3-9D05AA83-872631F1'}
+        scrollingSpeed = {1000}
+        verticallyCentered = {true}
+    
+        render={({ state, fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <Homepage/>
+            </ReactFullpage.Wrapper>
+          )
         }}
       />
-    </main>
-  </div>
-)
+    )
+  }
+}
 
 export default Home
